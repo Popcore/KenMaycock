c250e30f19693d8e75cf6c5dce786a68b4d21776
Popcore <smallzed@gmail.com>
2015-05-22 12:24:16 +0100

    fully implemented mobile navigation and overlay

--- wp-content/themes/kenmaycock_1.0/library/js/km-script.js
+++ wp-content/themes/kenmaycock_1.0/library/js/km-script.js
@@ -3,15 +3,25 @@ jQuery(window).load( function() {
 	console.log('rock n roll');
 
 	// Mobile open/close menu
-	$('#menu-icon').on('click', function() {
-		var slideMenu = $('aside#slide-menu');		
+	$('.menu-icon-wrapper').on('click', function() {
+		var slideMenu = $('aside#slide-menu'),
+				$this = $(this);
 
+		// change icon	
+		if($this.hasClass('close-menu-icon')) {
+			$this.removeClass('close-menu-icon');
+		} else {
+			$this.addClass('close-menu-icon');
+		}
+
+		// animate menu and overlay
 		if(slideMenu.hasClass('open-slide-menu')) {
 			slideMenu.removeClass('open-slide-menu');
 			$('body').removeClass('inactive');
+			$('#full-overlay').remove();
 		} else {
 			slideMenu.css('visibility', 'visible').addClass('open-slide-menu');
-			$('body').addClass('inactive');
+			$('body').addClass('inactive').append('<div id="full-overlay"></div>');
 		}
 		
 	});
